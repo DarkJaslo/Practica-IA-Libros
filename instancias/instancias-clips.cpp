30fe95ea5a7788cb3d9da5f439e3baf2250f208f
Jon <jon.campillo@gmail.com>
2023-12-02 13:04:13 +0100

    Formateador de instancias a CLIPS

---
+++ instancias/instancias-clips.cpp
@@ -0,0 +1,362 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+map<string,string> titulosClips;
+map<string,string> titulosNormal;
+map<string,string> publicadorasClips;
+map<string,string> publicadorasNormal;
+map<string,string> autoresClips;
+map<string,string> autoresNormal;
+set<string> generos;
+set<string> temas;
+
+void inserta(map<string,string>& m1, map<string,string>& m2, const string& key, const string& value)
+{
+    if(m1.find(key) != m1.end())
+    {
+        if(m1[key] != value) //problema
+        {
+            cerr << "Error: el nombre " << key << " ya se esta traduciendo como " << m1[key] << ", pero se quiere traducir por " << value << "\n";
+            __throw_runtime_error("excepcion");
+        }
+    }
+    else
+    {
+        m1[key] = value;
+        m2[value] = key;
+    }
+}
+
+void add(set<string>& set, const string& item)
+{
+    if(set.find(item) != set.end()) return;
+
+    set.insert(item);
+}
+
+string normToClips(const string& nombre)
+{
+    string res;
+    res.reserve(nombre.length()+1);
+
+    for(char c : nombre)
+    {
+        if(c >= 'A' and c <= 'Z') //mayus
+        {
+            c += ('a'-'A');
+        }
+        else if(c == 32) //space
+        {
+            c = '-';
+        }
+        else if(c == ',' or c == '.')
+            continue;
+
+        res += c;
+    }
+
+    return res;
+}
+
+string tipo(const string& t)
+{
+    if(t == "o")
+    {
+        return "One-shot";
+    }
+    else if(t == "s")
+    {
+        return "Serializado";
+    }
+    cerr << t << "\n";
+    __throw_domain_error("mal input en tipo()");
+}
+
+string dif(const string& d)
+{
+    if(d == "b"){
+        return "baja";
+    }
+    else if(d == "m"){
+        return "media";
+    }
+    else if(d == "a"){
+        return "alta";
+    }
+    cerr << d << "\n";
+    __throw_domain_error("mal input en dif()");
+}
+
+string estado(const string& e)
+{
+    if(e == "ac"){
+        return "acabado";
+    }
+    else if(e == "pu"){
+        return "en publicacion";
+    }
+    else if(e == "pa"){
+        return "en pausa";
+    }
+    else if(e == "ca"){
+        return "cancelado";
+    }
+    cerr << e << "\n";
+    __throw_domain_error("mal input en estado()");
+}
+
+string frec(const string& f)
+{
+    //1s|2s|1m|2m|3m|6m|ir {"semanal" "quincenal" "mensual" "bimestral" "trimestral" "semestral" "irregular"}
+    if(f == "1s"){
+        return "semanal";
+    }
+    else if(f == "2s"){
+        return "quincenal";
+    }
+    else if(f == "1m"){
+        return "mensual";
+    }
+    else if(f == "2m"){
+        return "bimestral";
+    }
+    else if(f == "3m"){
+        return "trimestral";
+    }
+    else if(f == "6m"){
+        return "semestral";
+    }
+    else if(f == "ir"){
+        return "irregular";
+    }
+    cerr << f << "\n";
+    __throw_domain_error("mal input en frec()");
+}
+
+string metodo(const string& m)
+{
+    if(m == "d"){
+        return "digital";
+    }
+    else if(m == "f"){
+        return "fisico";
+    }
+    else if(m == "a"){
+        return "ambos";
+    }
+    cerr << m << "\n";
+    __throw_domain_error("mal input en metodo()");
+}
+
+string precio(const string& p)
+{
+    if(p == "g"){
+        return "gratuito";
+    }
+    else if(p == "p"){
+        return "de pago";
+    }
+    cerr << p << "\n";
+    __throw_domain_error("mal input en precio()");
+}
+
+struct Manga
+{
+    string titulo; //Normal
+    string tipo;
+    int tomos;
+    int capitulos;
+    string autor; //Clips
+    string ilustrador; //Clips
+    string publicadora; //Clips
+    int copias;
+    string dificultad;
+    string estado;
+    string frecuencia;
+    string ini;
+    string metodo;
+    string precio;
+    bool anime;
+    float valoracion;
+    int edad;
+    vector<string> generos;
+    vector<string> temas;
+
+    string generosToClips()
+    {
+        string res;
+        for(const string& g : generos)
+        {
+            res.append("  [");
+            res.append(normToClips(g));
+            res.append("]");
+        }
+        return res;
+    }
+
+    string temasToClips()
+    {
+        string res;
+        for(const string& t : temas)
+        {
+            res.append("  [");
+            res.append(normToClips(t));
+            res.append("]");
+        }
+        return res;
+    }
+
+    string tieneAnime(){
+        if(anime) return "true";
+        return "false";
+    }
+
+    void toClips()
+    {
+        cout << "(";
+        cout << "[" << titulosClips[titulo] << "] of " << tipo << "\n";
+        cout << "\t(titulo  \"" << titulo << "\")\n";
+        cout << "\t(publicado-por  [" << publicadora << "])\n";
+        if(generos.size() > 0)
+            cout << "\t(pertenece-a" << generosToClips() << ")\n";
+        if(temas.size() > 0)
+            cout << "\t(trata-de" << temasToClips() << ")\n";
+        cout << "\t(tomos  " << tomos << ")\n";
+        cout << "\t(capitulos  " << capitulos << ")\n";
+        cout << "\t(copias-vendidas  " << copias << ")\n";
+        cout << "\t(dificultad-lectura  \"" << dificultad << "\")\n";
+        cout << "\t(estado-publicacion  \"" << estado << "\")\n";
+        cout << "\t(frecuencia-publicacion  \"" << frecuencia << "\")\n";
+        cout << "\t(inicio-publicacion  \"" << ini << "\")\n";
+        cout << "\t(metodo-distribucion  \"" << metodo << "\")\n";
+        cout << "\t(precio  \"" << precio << "\")\n";
+        cout << "\t(tiene-anime  \"" << tieneAnime() << "\")\n";
+        cout << "\t(valoracion  " << valoracion << ")\n";
+        cout << "\t(restriccion-edad  " << edad << ")\n";
+        cout << ")\n";
+    }    
+};
+
+void readManga(Manga& m)
+{
+    string l;
+    int n;
+
+    //Lee titulo
+    getline(cin, l);
+
+    m.titulo = l;
+    inserta(titulosClips,titulosNormal,l,normToClips(l));
+
+    //Lee autor
+    getline(cin, l);
+    {
+        string aux = normToClips(l);
+        inserta(autoresClips,autoresNormal,l,aux);
+        m.autor = aux;
+    }
+
+    //Lee ilustrador
+    getline(cin, l);
+    {
+        string aux = normToClips(l);
+        inserta(autoresClips,autoresNormal,l,aux);
+        m.ilustrador = aux;
+    }
+
+    //Lee publicadora
+    getline(cin, l);
+    {
+        string aux = normToClips(l);
+        inserta(publicadorasClips,publicadorasNormal,l,aux);
+        m.publicadora = aux;
+    }
+
+    //Lee generos
+    cin >> n;
+    //Lee n generos
+    m.generos = vector<string>(n);
+    getline(cin,l);
+    for(int i = 0; i < n; ++i)
+    {
+        getline(cin,m.generos[i]);
+        add(generos,m.generos[i]);
+    }
+
+    //Lee temas
+    cin >> n;
+    //Lee n temas
+    m.temas = vector<string>(n);
+    getline(cin,l);
+    for(int i = 0; i < n; ++i)
+    {
+        getline(cin,m.temas[i]);
+        add(temas,m.temas[i]);
+    }
+    
+    //Lee tipo
+    cin >> l;
+    m.tipo = tipo(l);
+
+    //Lee tomos
+    cin >> n;
+    m.tomos = n;
+
+    //Lee capitulos
+    cin >> n;
+    m.capitulos = n;
+
+    //Lee copias vendidas
+    cin >> n;
+    m.copias = n;
+
+    //Lee dificultad
+    cin >> l;
+    m.dificultad = dif(l);
+
+    //Lee estado publicacion
+    cin >> l;
+    m.estado = estado(l);
+
+    //Lee frecuencia de publicacion
+    cin >> l;
+    m.frecuencia = frec(l);
+
+    //Lee fecha inicio
+    cin >> l;
+    m.ini = l;
+
+    //Lee metodo
+    cin >> l;
+    m.metodo = metodo(l);
+
+    //Lee precio
+    cin >> l;
+    m.precio = precio(l);
+
+    //Lee anime
+    cin >> l;
+    l == "true" ? m.anime = true : m.anime = false;
+
+    //Lee valoracion
+    {
+        float aux;
+        cin >> aux;
+        m.valoracion = aux;
+    }
+
+    //Lee edad
+    cin >> n;
+    m.edad = n;
+}
+
+int main()
+{
+    Manga m;
+    readManga(m);
+    m.toClips();
+}
